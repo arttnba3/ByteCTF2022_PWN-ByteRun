86327c64fcd7fce66c5c2168686d39f94a5feaf1
lingzongyu <lingzongyu@bytedance.com>
2022-08-08 20:09:43 +0800

    add basic env

---
+++ environ/pc-bios/optionrom/linuxboot_dma.d
@@ -0,0 +1,14 @@
+linuxboot_dma.o: \
+ /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/linuxboot_dma.c \
+ /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom.h \
+ /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/standard-headers/linux/qemu_fw_cfg.h \
+ /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/include/standard-headers/linux/types.h \
+ /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom_fw_cfg.h
+
+/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom.h:
+
+/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/standard-headers/linux/qemu_fw_cfg.h:
+
+/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/include/standard-headers/linux/types.h:
+
+/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom_fw_cfg.h:
---
+++ environ/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+./qemu-system-x86_64 \
+    -kernel ./bzImage \
+    -append "root=/dev/ram console=ttyS0 oops=panic panic=1 loglevel=3 quiet kaslr" \
+    -cpu kvm64,+smep,+smap \
+    -smp cores=2,threads=2 \
+    -initrd ./rootfs.cpio \
+    -m 128M \
+    -nographic \
+    -monitor /dev/null \
+    -device byte_dev-pci
