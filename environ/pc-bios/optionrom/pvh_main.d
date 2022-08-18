f6b380e449a9e49064a585a2e4019fa5d9756b94
arttnba3 <1543127579@qq.com>
2022-08-18 09:50:07 -0700

    fix bugs in ring buffer queue

--- environ/pc-bios/optionrom/pvh_main.d
+++ environ/pc-bios/optionrom/pvh_main.d
@@ -1,17 +1,17 @@
 pvh_main.o: \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/pvh_main.c \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom.h \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/standard-headers/linux/qemu_fw_cfg.h \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/include/standard-headers/linux/types.h \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom_fw_cfg.h \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/hw/xen/start_info.h
+ /home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/pvh_main.c \
+ /home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom.h \
+ /home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/standard-headers/linux/qemu_fw_cfg.h \
+ /home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/include/standard-headers/linux/types.h \
+ /home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom_fw_cfg.h \
+ /home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/hw/xen/start_info.h
 
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom.h:
+/home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom.h:
 
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/standard-headers/linux/qemu_fw_cfg.h:
+/home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/standard-headers/linux/qemu_fw_cfg.h:
 
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/include/standard-headers/linux/types.h:
+/home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/include/standard-headers/linux/types.h:
 
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom_fw_cfg.h:
+/home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optrom_fw_cfg.h:
 
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/hw/xen/start_info.h:
+/home/arttnba3/Desktop/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/../../include/hw/xen/start_info.h:
