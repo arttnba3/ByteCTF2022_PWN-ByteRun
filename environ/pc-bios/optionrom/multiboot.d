dcd7d580e527a7cf7f37edd7b5e83fc50bd6752a
arttnba3 <lingzongyu@bytedance.com>
2022-08-23 21:40:54 +0800

    fix BIOS

--- environ/pc-bios/optionrom/multiboot.d
+++
@@ -1,5 +0,0 @@
-multiboot.o: \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/multiboot.S \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optionrom.h
-
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optionrom.h:
--- environ/pc-bios/optionrom/pvh.d
+++
@@ -1,5 +0,0 @@
-pvh.o: \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/pvh.S \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optionrom.h
-
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optionrom.h:
