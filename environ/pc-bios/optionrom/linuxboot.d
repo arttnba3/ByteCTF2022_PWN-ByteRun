cbaf975f8a3e17edd145badbe32ae22476163e6c
arttnba3 <arttnba3@outlook.com>
2022-08-23 21:40:54 +0800

    fix BIOS

--- environ/pc-bios/optionrom/linuxboot.d
+++
@@ -1,5 +0,0 @@
-linuxboot.o: \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/linuxboot.S \
- /home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optionrom.h
-
-/home/lingzongyu/bytedance/ByteCTF2022_PWN-ByteChain/qemu/qemu-7.0.0/pc-bios/optionrom/optionrom.h:
